7169f03cdb0a04f68b2ed2199614c5636c0e82d0
Aditya Kumar <100465366+adityapal87@users.noreply.github.com>
2022-06-07 10:41:26 +0530

    added NameCard folder

---
+++ src/components/NameCard/NameCard.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './NameCard.css'
+
+const nameCheapUrl="https://www.namecheap.com/domains/registration/results/?domain=%27%3B"
+
+const NameCard = ({suggestedName})=>{
+    return(
+       <a className='card-link' href={`${nameCheapUrl}${suggestedName}`} 
+       rel="noreferrer"
+       target ="_blank">
+            <div className='result-name-card'>
+            <p className="result-name">{suggestedName}</p>
+        </div>
+       </a>
+    )
+}
+export default NameCard;
